54daf609618690b7b7b80561848f8f4d64443a82
Krittiya Clark <krittiyaclark@gmail.com>
2017-09-29 10:19:42 -0400

    Added a nav bar to a recipe app

--- recipe-prop-starter/src/RecipeApp.js
+++ recipe-prop-starter/src/RecipeApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar';
 import Recipe from './Recipe';
 import './RecipeApp.css';
 
@@ -6,6 +7,7 @@ class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <Recipe
           title='pasta'
           ingredients={['flour', 'water']}
